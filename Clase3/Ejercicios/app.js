d549901cd466be6c9381ba014af672d727c62e6d
MaliSoi <malalasoifer@gmail.com>
2025-07-26 11:39:01 -0300

    Ejercicios de la clase 3

---
+++ Clase3/Ejercicios/app.js
@@ -0,0 +1,30 @@
+const title = document.createElement('h1');                      //Crea el h1
+const text = document.createTextNode("¡Bienvenida!");            //Crear el nodo de texto
+title.appendChild(text);                                      //Insertar el texto dentro del h1
+document.body.appendChild(title);                             // Insertar el h1 en el body
+
+
+//Ejercicio 2
+/*const lista = document.getElementById('compras');
+
+const item1 = document.createElement('li');
+const text1 = document.createTextNode('Leche')
+        item1.appendChild(text1);
+        lista.appendChild(item1);
+
+const item2 = document.createElement('li');
+const text2 = document.createTextNode('Queso')
+        item2.appendChild(text2);
+        lista.appendChild(item2);*/
+        
+const lista = document.getElementById('compras');
+
+function agregarItem(texto) {
+    const li = document.createElement('li');
+    const nodoTexto = document.createTextNode(texto);
+    li.appendChild(nodoTexto);
+    lista.appendChild(li)
+}
+
+agregarItem('Leche');
+agregarItem('Queso');
