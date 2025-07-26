85bf4637724ff51021b2d073450263b52890f9b1
MaliSoi <malalasoifer@gmail.com>
2025-07-26 12:06:16 -0300

    Agregando ejercicios 4 y 5

--- Clase3/Ejercicios/app.js
+++ Clase3/Ejercicios/app.js
@@ -28,3 +28,41 @@ function agregarItem(texto) {
 
 agregarItem('Leche');
 agregarItem('Queso');
+
+//Ejercicio 3
+
+/*const mensaje = document.querySelector('.mensaje'); // el . indica que es una clase
+mensaje.innerHTML = 'Hola, Javascript 👋';*/
+
+//Ejercicio 4
+
+const nombre = "Arnold";
+const edad = 32;
+
+const mensaje = `Hola, mi nombre es ${nombre} y tengo ${edad} años.`;//Creando el mensaje con template string
+
+const parrafo = document.createElement('p'); //creando un elemento <p>
+
+const texto = document.createTextNode(mensaje); //creando el nodo de texto con el mensaje
+
+parrafo.appendChild(texto); //insertando el texto en el <p>
+
+document.body.appendChild(parrafo); //insertando el <p> en el body del documento
+
+//Ejercicio 5
+
+
+
+function escribir (titulo, texto){      //Insertar en el body con innerHTML y template strings
+document.body.innerHTML += `
+<h2>${titulo}</h2>
+<p> ${texto}</p>
+`;
+}
+
+//Llamamos a la funcion con valores
+
+escribir("Titulo dinámico", "Este contenido fue generado desde JS.");
+
+
+
